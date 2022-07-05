9d0be8ab01b6f2a21a10483914c48663fe4faf3a
Timur Iskhodzhanov <timurrrr@gmail.com>
2022-07-05 15:06:52 -0700

    Add a fake 2022 Toyota GR86

--- examples/FakeToyotaGR86/FakeToyotaGR86.ino
+++ examples/FakeToyotaGR86/FakeToyotaGR86.ino
@@ -2,7 +2,7 @@
 
 // This is a demo program that sends messages over the CAN bus in
 // a way that resembles real messages you can receive if you listen
-// to messages on the CAN bus of a 2013-2020 Subaru BRZ.
+// to messages on the CAN bus of a 2022 Toyota GR86.
 //
 // DO NOT USE IT IN THE CAN NETWORK OF A REAL VEHICLE as it can cause unexpected
 // side effects.
@@ -119,23 +119,20 @@ void loop() {
   // Many PIDs are sent 50 times per second.
   uint16_t num_cycles_per_second = 50;
 
-  // 0x18, 0x140, 0x141 and 0x142 are intentionally duplicated in this array as they are sent 100 times
+  // 0x40 and 0x41 are intentionally duplicated in this array as they are sent 100 times
   // per second (double that for other PIDs) in the real car.
   uint16_t pids[] = {
     // These are sent 100 times per second:
-    0x18, 0x140, 0x141, 0x142,
-    // These are sent 50 times per second:
-    0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x144, 0x152, 0x156, 0x280,
+    0x40, 0x41,
+    // These are sent 50 times per second, part 1:
+    0x118, 0x138, 0x139, 0x13B, 0x13C,
+    // These are sent 100 times per second (duplicates):
+    0x40, 0x41,
+    // These are sent 50 times per second, part 2:
+    0x143, 0x146,
     // TODO: These are actually sent less frequently than 50 times per second:
-    0x282, // 16.7 times per second
-    0x284, // 10 times per second
-    0x360, // 20 times per second
-    // These are commented out so that we don't send way too many messages:
-    //0x361, // 20 times per second
-    //0x370, // 20 times per second
-    //0x372, // 10 times per second
-    // These are sent 100 times per second:
-    0x18, 0x140, 0x141, 0x142,
+    0x241, // 20 times per second
+    0x345, // 10 times per second
   };
   uint16_t num_messages_per_cycle = sizeof(pids) / sizeof(pids[0]);
   uint16_t num_messages_per_second =
@@ -201,39 +198,6 @@ void try_to_receive_data() {
     data[data_length++] = byte_read;
   }
 
-  if (id == 0x7C0 && data[0] == 0x2 && data[1] == 0x21 && data[2] == 0x29) {
-    // 0x7C0 / 0x2129 — Returns fuel level in liters x2.
-    uint8_t response[8] = {0};
-    response[0] = 0x3;
-    response[1] = 0x61;
-    response[2] = 0x29;
-    response[3] = 0x1C;
-    send_data(0x7C8, response, 8);
-    return;
-  }
-
-  if (id == 0x7DF && data[0] == 0x2 && data[1] == 0x01 && data[2] == 0x0f) {
-    // 0x7DF / 0x010F — Returns (intake temperature in ºC + 40)
-    uint8_t response[8] = {0};
-    response[0] = 0x3;
-    response[1] = 0x41;
-    response[2] = 0x0f;
-    response[3] = 40 + 36;  // 36 ºC
-    send_data(0x7E8, response, 8);
-    return;
-  }
-
-  if (id == 0x7DF && data[0] == 0x2 && data[1] == 0x01 && data[2] == 0x46) {
-    // 0x7DF / 0x0146 — Returns (air temperature in ºC + 40)
-    uint8_t response[8] = {0};
-    response[0] = 0x3;
-    response[1] = 0x41;
-    response[2] = 0x46;
-    response[3] = 40 + 27;  // 27 ºC
-    send_data(0x7E8, response, 8);
-    return;
-  }
-
   if (id == 0x750 && data_length >= 1 && data[0] == 0x2a) {
     if (data_length >= 3 && data[1] == 0x02 && data[2] == 0x21) {
       if (data[3] == 0x30) {
@@ -291,78 +255,11 @@ void generate_payload(uint16_t pid, uint8_t *payload) {
   memset(payload, /* value= */ 0, /* size= */ 8);
 
   switch (pid) {
-    case 0xD0: {
-      // 0xD0 contains the steering wheel angle and data from motion sensors.
-
-      // Pretend that the steering wheel is turned by 123 degrees to the left
-      int16_t steering_angle_degrees = -123;
-      int16_t value = steering_angle_degrees * 10;
-      payload[0] = value & 0xFF;
-      payload[1] = (value >> 8) & 0xFF;
-
-      // TODO: Verify the scale for this value. The current scale is suspicious,
-      // but matches real-world testing so far. Need to go to a skid pad to
-      // verify for sure.
-      int16_t rotation_clockwise_degrees_per_second = -70;
-      int16_t rotation_clockwise_radians_per_second_x180 =
-          (int16_t)(-3.14159 * rotation_clockwise_degrees_per_second);
-      payload[2] = rotation_clockwise_radians_per_second_x180 & 0xFF;
-      payload[3] = (rotation_clockwise_radians_per_second_x180 >> 8) & 0xFF;
-
-      // TODO: decode what's in payload[4] and payload[5].
-
-      float lateral_acceleration_g = 0.3;
-      // Looks to be encoded in a way that +1 increment is +0.2 m/s2.
-      payload[6] = (int8_t)(9.80665 * lateral_acceleration_g / 0.2);
-
-      float longitudinal_acceleration_g = 0.2;
-      // Looks to be encoded in a way that +1 increment is -0.1 m/s2.
-      // I know, it's strange that they use different scales for lat vs long.
-      payload[7] = (int8_t)(-9.80665 * longitudinal_acceleration_g / 0.1);
-      break;
-    }
-
-    case 0xD1: {
-      // 0xD1 contains the speed, and the master brake cylinder pressure.
-      uint16_t speed_m_s = 10;  // 36 km/h, ~22.4 mph.
-      // The encoding seems to be roughly radians per second x100.
-      // The coefficient was tuned by comparing the values against an external
-      // GPS from a session where I drove in a straight line on a highway at
-      // constant speed on cruise control.
-      uint16_t speed_value = (uint16_t)(speed_m_s * 63.72);
-      payload[2] = speed_value & 0xFF;
-      payload[3] = (speed_value >> 8) & 0xFF;
-
-      // The units used for the master brake cylinder pressure are believed to
-      // be 1/128 kPa.
-      float brake_pressure_kPa = 1024;
-      payload[2] = (uint8_t)(brake_pressure_kPa / 128);
-      break;
-    }
-
-    case 0xD4: {
-      // Wheel speed sensors / ABS sensors.
-      uint16_t value = (uint16_t)(10 * 61);
-      payload[0] = value & 0xFF;
-      payload[1] = (value >> 8) & 0xFF;
-
-      value = (uint16_t)(10 * 62);
-      payload[2] = value & 0xFF;
-      payload[3] = (value >> 8) & 0xFF;
-
-      value = (uint16_t)(10 * 63);
-      payload[4] = value & 0xFF;
-      payload[5] = (value >> 8) & 0xFF;
-
-      value = (uint16_t)(10 * 64);
-      payload[6] = value & 0xFF;
-      payload[7] = (value >> 8) & 0xFF;
-      break;
-    }
-
-    case 0x140: {
+    case 0x40: {
       uint8_t accelerator_pedal_percent = 42;
-      payload[0] = accelerator_pedal_percent * 255 / 100;
+      payload[4] = accelerator_pedal_percent * 255 / 100;
+      payload[5] = payload[4];
+      payload[6] = payload[4];
 
       // The clutch pedal has two sensors:
       // - 0% and >0% (used here)
@@ -378,12 +275,48 @@ void generate_payload(uint16_t pid, uint8_t *payload) {
       break;
     }
 
-    case 0x360: {
+
+    case 0x138: {
+      // Pretend that the steering wheel is turned by 123 degrees to the left
+      int16_t steering_angle_degrees = -123;
+      int16_t steering_value = steering_angle_degrees * 10;
+      payload[2] = steering_value & 0xFF;
+      payload[3] = (steering_value >> 8) & 0xFF;
+
+      // TODO: Verify the scale for this value. The current scale is suspicious,
+      // but matches real-world testing so far. Need to go to a skid pad to
+      // verify for sure.
+      float yaw_rate_degrees_per_second = -12.3;
+      int16_t yaw_rate_value = (int16_t)(-yaw_rate_degrees_per_second / 0.2725);
+      payload[4] = yaw_rate_value & 0xFF;
+      payload[5] = (yaw_rate_value >> 8) & 0xFF;
+      break;
+    }
+
+    case 0x139: {
+      uint16_t speed_m_s = 10;  // 36 km/h, ~22.4 mph.
+      // The encoding seems to be roughly radians per second x100.
+      // The coefficient was tuned by comparing the values against an external
+      // GPS from a session where I drove in a straight line on a highway at
+      // constant speed on cruise control.
+      uint16_t speed_value = (uint16_t)(speed_m_s * 63.72);
+      payload[2] = speed_value & 0xFF;
+      payload[3] = (speed_value >> 8) & 0xFF;
+
+      // The units used for the master brake cylinder pressure are believed to
+      // be 1/128 kPa.
+      float brake_pressure_kPa = 1024;
+      payload[4] = 0x0C;
+      payload[5] = (uint8_t)(brake_pressure_kPa / 128);
+      break;
+    }
+
+    case 0x345: {
       uint8_t oil_temperature_celsius = 100;
-      payload[2] = oil_temperature_celsius + 40;
+      payload[3] = oil_temperature_celsius + 40;
 
       uint8_t coolant_temperature_celsius = 90;
-      payload[3] = coolant_temperature_celsius + 40;
+      payload[4] = coolant_temperature_celsius + 40;
       break;
     }
   }
